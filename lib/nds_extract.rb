9f597e40849413fe34db71ee925f45b69f79bbd9
Owen Cruse <ocruse7@gmail.com>
2019-10-25 18:12:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,18 +4,14 @@ require 'pp'
 
 def directors_totals(nds)
   total = 0
-  i = 0 
   j = 0
   directors_hash = {}
-  while i < nds.length do
-    while j < nds[i][:movies].length do
+  nds.length.times do |i|
+    nds[i][:movies].length.times do |j|
       money = nds[i][:movies][j][:worldwide_gross]
       total += money
-      j += 1
     end
     directors_hash[nds[i][:name]] = total
-    i += 1
-    j = 0
     total = 0
   end
   return directors_hash
